23934d5af60cc59e49147fe499bd6221131be6f5
MrWalshyType2 <mwalsh@qa.com>
2022-10-20 15:39:15 +0100

    Added constructors and methods to Car class example

--- src/main/java/com/qa/lbg_examples/classes_and_objects/Car.java
+++ src/main/java/com/qa/lbg_examples/classes_and_objects/Car.java
@@ -5,11 +5,56 @@ public class Car {
 	// Properties/state/instance variables of a class
 	// AccessModifier DataType name;
 	public String manufacturer;
-	public int numberOfWheels; 
+	public int numberOfWheels;
+	public int airRatio = 19;
+	public int fuelRatio = 11;
 	
 	// All instance variables have a default value
 	// - Reference types default to the 'null' value
 	// - Primitive numerical types to 0
 	// - Primitive char to \u0000
 	// - Primitive boolean to false
+	
+	// Constructor
+	// - name must match the name of the class
+	// - there is no return type
+	//   - the return type is implicit, it is the class itself
+	public Car() {
+		super();
+	}
+	
+	// - we can use constructors to accept input for initialising
+	//   the properties of an object
+	// - we can have more than one constructor, constructor overloading
+	// - if we have more than one constructor, it must have different
+	//   parameter types
+	public Car(String manufacturer, int numberOfWheels) {
+		// we have a special keyword called 'this' which is used
+		// to refer to the current object
+		// - this.manufacturer is referring to the variable on line 7
+		this.manufacturer = manufacturer;
+		this.numberOfWheels = numberOfWheels;
+	}
+
+	// Methods
+	// - a non static method is an instance method, it belongs
+	//   to an object and can only be called from an object
+	// - instance methods represent the behaviours of an object
+	public void accelerate(int percent) {
+		if (percent < 25) {
+			airRatio = 19;
+			System.out.println(manufacturer + " Slow acceleration or stopped");
+		} else if (percent < 75) {
+			airRatio = 15;
+			System.out.println(manufacturer + " Medium acceleration");
+		} else {
+			airRatio = 11;
+			System.out.println(manufacturer + " Fast acceleration");
+		}
+	}
+	
+	public String getInformation() {
+		return "MANUFACTURER=" + manufacturer + ", AIR:FUEL RATIO: " + 
+				airRatio + ":" + fuelRatio;
+	}
 }
--- src/main/java/com/qa/lbg_examples/classes_and_objects/Runner.java
+++ src/main/java/com/qa/lbg_examples/classes_and_objects/Runner.java
@@ -22,6 +22,23 @@ public class Runner {
 		System.out.println("=== AFTER ===");
 		System.out.println(mazda.manufacturer);
 		System.out.println(mazda.numberOfWheels);
+		
+		// We can use a constructor to initialise a new Car
+		// object with some data
+		System.out.println("=== USING A CONSTRUCTOR ===");
+		Car toyota = new Car("Toyota", 4);
+		System.out.println(toyota.manufacturer);
+		System.out.println(toyota.numberOfWheels);
+		
+		// We can use methods (behaviours) on instances of a class
+		System.out.println("=== USING METHODS ===");
+		mazda.accelerate(13);
+		toyota.accelerate(90);
+		
+		String mazdaInfo = mazda.getInformation();
+		String toyotaInfo = toyota.getInformation();
+		System.out.println(mazdaInfo);
+		System.out.println(toyotaInfo);
 	}
 
 }
