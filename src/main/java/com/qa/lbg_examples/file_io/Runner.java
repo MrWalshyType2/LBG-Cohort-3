d7be4367e0a2000168804bc90e745a84a7595033
MrWalshyType2 <mwalsh@qa.com>
2022-10-25 12:21:05 +0100

    Added creating a file and writing to file with output stream examples

--- src/main/java/com/qa/lbg_examples/file_io/Runner.java
+++ src/main/java/com/qa/lbg_examples/file_io/Runner.java
@@ -2,31 +2,73 @@ package com.qa.lbg_examples.file_io;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 public class Runner {
 
 	public static void main(String[] args) {
-		
+		//simplifiedReadExample("C:\\Users\\Morgan Walsh\\eclipse-workspace\\lbg_examples\\src\\main\\java\\com\\qa\\lbg_examples\\file_io\\text_file.txt");
+		//complexReadExample();
+//		tryReadWithResources();
+//		writeTextFile("C:\\Users\\Morgan Walsh\\eclipse-workspace\\lbg_examples\\src\\main\\java\\com\\qa\\lbg_examples\\file_io\\other_text_file.txt", "Hello\nWorld!");
+//		createFile("C:\\Users\\Morgan Walsh\\eclipse-workspace\\lbg_examples\\src\\main\\java\\com\\qa\\lbg_examples\\file_io\\new_text_file.txt");
+		writeToFileWithFileOutputStream("C:\\Users\\Morgan Walsh\\eclipse-workspace\\lbg_examples\\src\\main\\java\\com\\qa\\lbg_examples\\file_io\\new_text_file.txt", "Test\nHello");
 	}
-
+	
+	public static void writeTextFile(String filename, String content) {
+		try {
+			FileWriter wr = new FileWriter(filename);
+			BufferedWriter bw = new BufferedWriter(wr);
+			bw.write(content);
+			bw.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void writeToFileWithFileOutputStream(String filename, String content) {
+		try (OutputStream os = new FileOutputStream(new File(filename))) {
+			os.write(content.getBytes());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void createFile(String filename) {
+		try {
+			File file = new File(filename);
+			
+			if (file.createNewFile()) System.out.println("File created successfully.");
+			else System.out.println("File already exists");
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+	
 	public static void simplifiedReadExample(String filename) {
 		try {
 			FileReader fr = new FileReader(filename);
 			BufferedReader br = new BufferedReader(fr);
+			// Input and Output stream readers in Java make use of the decorator pattern
+			
 			String line;
 
 			while (br.ready()) {
 				line = br.readLine();
 				System.out.println(line);
 			} // end of while loop
-			br.close();
+			br.close(); // close the input stream
 		} catch (Exception e) { // this will catch any exception
 			System.out.println(e.getMessage());
 		} // end of catch clause
@@ -60,6 +102,7 @@ public class Runner {
 			// when trying to read the file
 			System.out.println("No input stream available.");
 		} finally {
+			// finally block is commonly used for cleaning up resources
 			if (reader != null) {
 				try {
 					reader.close();
@@ -87,14 +130,4 @@ public class Runner {
 		}
 	}
 
-	public void writeTextFile(String filename, String content) {
-		try {
-			FileWriter wr = new FileWriter(filename);
-			BufferedWriter bw = new BufferedWriter(wr);
-			bw.write(content);
-			bw.close();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
 }
