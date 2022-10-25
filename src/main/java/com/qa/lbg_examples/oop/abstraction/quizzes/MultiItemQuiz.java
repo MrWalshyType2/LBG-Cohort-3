77a90e16b26a0bbc59cf173de6a5e7335585536b
MrWalshyType2 <mwalsh@qa.com>
2022-10-25 14:57:40 +0100

    Added quiz example with abstraction applied

---
+++ src/main/java/com/qa/lbg_examples/oop/abstraction/quizzes/MultiItemQuiz.java
@@ -0,0 +1,31 @@
+package com.qa.lbg_examples.oop.abstraction.quizzes;
+
+import java.util.Random;
+
+import com.qa.lbg_examples.oop.abstraction.Question;
+
+public class MultiItemQuiz extends Quiz {
+
+	private Question[] questions;
+	
+	public MultiItemQuiz(Question[] questions) {
+		if (questions == null) throw new IllegalArgumentException("Question must not be null");
+		if (questions.length == 0) throw new IllegalArgumentException("Must be at least one question.");
+		this.questions = questions;
+	}
+	
+	@Override
+	protected Question getQuestion() {
+		Random random = new Random();
+		int randomNumber = random.nextInt(questions.length);
+		return questions[randomNumber];
+	}
+
+	@Override
+	protected boolean checkAnswer(Question question, String answer) {
+		for (Question q : questions) {
+			if (q.equals(question)) return question.checkAnswer(answer);
+		}
+		throw new RuntimeException("Invalid question supplied.");
+	}
+}
