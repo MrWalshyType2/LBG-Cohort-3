67f6daf082dc5e0f7ae53ce091779100d7e16d99
MrWalshyType2 <mwalsh@qa.com>
2022-10-25 11:14:36 +0100

    Added write example

--- src/main/java/com/qa/lbg_examples/file_io/Runner.java
+++ src/main/java/com/qa/lbg_examples/file_io/Runner.java
@@ -1,9 +1,11 @@
 package com.qa.lbg_examples.file_io;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,25 +13,25 @@ import java.io.InputStreamReader;
 public class Runner {
 
 	public static void main(String[] args) {
-
+		
 	}
 
 	public static void simplifiedReadExample(String filename) {
 		try {
-		      FileReader fr = new FileReader(filename);
-		      BufferedReader br = new BufferedReader(fr);
-		      String line;
+			FileReader fr = new FileReader(filename);
+			BufferedReader br = new BufferedReader(fr);
+			String line;
 
-		      while (br.ready()) {
-		          line = br.readLine();
-		          System.out.println(line);
-		      } // end of while loop
-		     br.close();
-		  } catch(Exception e){ // this will catch any exception
-		     System.out.println(e.getMessage());
-		  } // end of catch clause
+			while (br.ready()) {
+				line = br.readLine();
+				System.out.println(line);
+			} // end of while loop
+			br.close();
+		} catch (Exception e) { // this will catch any exception
+			System.out.println(e.getMessage());
+		} // end of catch clause
 	}
-	
+
 	public static void complexReadExample() {
 		// Load the file, for relative paths, the following method needs to be used
 		InputStream in = Runner.class.getResourceAsStream("./text_file.txt");
@@ -48,12 +50,14 @@ public class Runner {
 				System.out.println(line);
 			}
 		} catch (FileNotFoundException fne) {
-			// must be caught before
+			// must be caught before IOException as
+			// FileNotFoundException is a type of IOException
 			fne.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (NullPointerException npe) {
-			// triggered by InputStream in if file was not available
+			// triggered by 'InputStream in' if file was not available
+			// when trying to read the file
 			System.out.println("No input stream available.");
 		} finally {
 			if (reader != null) {
@@ -65,9 +69,12 @@ public class Runner {
 			}
 		}
 	}
-	
+
 	public static void tryReadWithResources() {
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(Runner.class.getResourceAsStream("./text_file.txt")))) {
+		// try with resources automatically calls the .close() method
+		// of resources implementing the AutoCloseable interface
+		try (BufferedReader reader = new BufferedReader(
+				new InputStreamReader(Runner.class.getResourceAsStream("./text_file.txt")))) {
 			String line = null;
 			while ((line = reader.readLine()) != null) {
 				System.out.println(line);
@@ -77,6 +84,17 @@ public class Runner {
 		} catch (NullPointerException npe) {
 			// triggered by InputStream in if file was not available
 			System.out.println("No input stream available.");
+		}
+	}
+
+	public void writeTextFile(String filename, String content) {
+		try {
+			FileWriter wr = new FileWriter(filename);
+			BufferedWriter bw = new BufferedWriter(wr);
+			bw.write(content);
+			bw.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 	}
 }
